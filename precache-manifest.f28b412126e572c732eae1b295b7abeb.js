f0d2e96613774c0ad102291e173b975f57773d04
Ayush <ayush4351@gmail.com>
2020-07-02 14:20:39 +0530

    Updates

---
+++ precache-manifest.f28b412126e572c732eae1b295b7abeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25097159e7519a6dcb9df0180a11ddbd",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "46331a400334a142affd",
+    "url": "/contact-manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "025dda55b951a940d6ed",
+    "url": "/contact-manager/static/css/main.ccead6ac.chunk.css"
+  },
+  {
+    "revision": "46331a400334a142affd",
+    "url": "/contact-manager/static/js/2.2d5bd23a.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/contact-manager/static/js/2.2d5bd23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025dda55b951a940d6ed",
+    "url": "/contact-manager/static/js/main.ebd3c9f9.chunk.js"
+  },
+  {
+    "revision": "f90dbfe40dcd70025833",
+    "url": "/contact-manager/static/js/runtime-main.f3461fd3.js"
+  }
+]);
